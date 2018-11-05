cb5172fddc1d1e6c6a64c44ed40885a457e54c36
User <rfedsxc@mail.ru>
2018-11-05 20:11:40 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        Students s1 = new Students("Alice", 19, "female", "USA");
+        Students s2 = new Students("John", 22, "male", "Canada");
+        Students s3 = new Students("Max", 36, "male", "Germany");
+        Students s4 = new Students("Kirill", 26, "male", "Russia");
+        Students s5 = new Students("Mila", 18, "female", "Kosovo");
+        Students s6 = new Students("Tanya", 24, "female", "Belarus");
+        Students s7 = new Students("Steven", 29, "male", "Australia");
+        Students s8 = new Students("Bob", 27, "male", "England");
+
+        ArrayList<Students> arrS = new ArrayList<>();
+        arrS.add(s1);
+        arrS.add(s2);
+        arrS.add(s3);
+        arrS.add(s4);
+        arrS.add(s5);
+        arrS.add(s6);
+        arrS.add(s7);
+        arrS.add(s8);
+
+        //Collections.sort(arrS, Students.COMPARE_BY_COUNTRY);
+        Collections.sort(arrS, Students.COMPARE_BY_AGE);
+        //Collections.sort(arrS, Students.COMPARE_BY_AGE);
+
+        for (int i = 0; i <arrS.size() ; i++) {
+            System.out.println(arrS.get(i).getAge());
+        }
+    }//main
+}//class
---
+++ src/com/company/Students.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.util.Comparator;
+
+public class Students implements Comparable {
+    private String name;
+    private int age;
+    private String sex;
+    private String country;
+
+    //to compare(sort) by default
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    //other ways to sort:
+    static Comparator<Students> COMPARE_BY_COUNTRY = new Comparator<Students>() {
+        public int compare(Students one, Students other) {
+            return one.getCountry().compareTo(other.getCountry());
+        }
+    };
+
+    static Comparator<Students> COMPARE_BY_NAME = new Comparator<Students>() {
+        public int compare(Students one, Students other) {
+            return one.getName().compareTo(other.getName());
+        }
+    };
+
+    static Comparator<Students> COMPARE_BY_AGE = new Comparator<Students>() {
+        public int compare(Students one, Students other) {
+            String age  = String.valueOf(one.getAge());
+            String age2  = String.valueOf(other.getAge());
+            return age.compareTo(age2);
+        }
+    };
+
+
+    Students(String name, int age, String sex, String country) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
